c814bd2c19cd76712d985bed8ec66b04041962f5
Ahmad Arif <ahmad.arif019@gmail.com>
2017-01-30 20:21:33 +0700

    using unofficial API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,34 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
+import {HackernewsApiService} from "./hackernews-api.service";
+import {MomentModule} from "angular2-moment/index";
+import { DomainPipe } from './domain.pipe';
+import { ItemCommentsComponent } from './item-comments/item-comments.component';
+import {routing} from "./app.routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent,
+    ItemComponent,
+    DomainPipe,
+    ItemCommentsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MomentModule,
+    routing
   ],
-  providers: [],
+  providers: [HackernewsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/stories/stories.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import {HackernewsApiService} from "../hackernews-api.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-stories',
+  templateUrl: './stories.component.html',
+  styleUrls: ['./stories.component.scss']
+})
+export class StoriesComponent implements OnInit {
+
+  typeSub: any;
+  pageSub: any;
+  items:any;
+  storiesType;
+  pageNum: number;
+  listStart: number;
+
+  constructor(
+    private hackerNewAPIService: HackernewsApiService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    // this.typeSub = this.route
+    //   .data
+    //   .subscribe(data => {
+    //     this.storiesType = (data as any).storiesType;
+    //   });
+
+    // this.pageSub = this.route.params.subscribe(params => {
+    //   this.pageNum = +params['page'] ? +params['page'] : 1;
+    //   this.hackerNewAPIService.fetchStories(this.storiesType, this.pageNum)
+    //     .subscribe(
+    //       response => this.items = response,
+    //       error => console.log('Error fetching ' + this.storiesType + ' stories'),
+    //       () => this.listStart = ((this.pageNum - 1) * 30) + 1
+    //     );
+    // });
+
+    this.storiesType = this.route.snapshot.data['storiesType'];
+    this.pageNum = this.route.snapshot.params['page'] ? this.route.snapshot.params['page'] : 1;
+    this.hackerNewAPIService.fetchStories(this.storiesType, this.pageNum)
+      .subscribe(
+        response => {
+          this.items = response;
+          console.log("reponse");
+        },
+        error => console.log('Error fetching ' + this.storiesType + ' stories'),
+        () => {
+          this.listStart = ((this.pageNum - 1) * 30) + 1;
+          console.log("listStart");
+        }
+      );
+  }
+
+}
